77a8225e46efc283881ee9ade61dc5ad0a80c129
Jakub Walczak <jakub.tomasz.walczak@gmail.com>
2023-01-20 12:50:29 +0100

    Enable and configure mongock

--- src/main/java/pl/walczak/jakub/mongodbmigrationspoc/MongodbMigrationsPocApplication.java
+++ src/main/java/pl/walczak/jakub/mongodbmigrationspoc/MongodbMigrationsPocApplication.java
@@ -1,9 +1,12 @@
 package pl.walczak.jakub.mongodbmigrationspoc;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableMongock
 public class MongodbMigrationsPocApplication {
 
     public static void main(String[] args) {
