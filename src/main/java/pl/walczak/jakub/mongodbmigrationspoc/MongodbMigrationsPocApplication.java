df7b48ad8c008122fa51c2fa72f49f8c1a07d01c
Jakub Walczak <jakub.tomasz.walczak@gmail.com>
2023-01-20 11:35:11 +0100

    initial commit

---
+++ src/main/java/pl/walczak/jakub/mongodbmigrationspoc/MongodbMigrationsPocApplication.java
@@ -0,0 +1,13 @@
+package pl.walczak.jakub.mongodbmigrationspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbMigrationsPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongodbMigrationsPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/walczak/jakub/mongodbmigrationspoc/MongodbMigrationsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.walczak.jakub.mongodbmigrationspoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbMigrationsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
